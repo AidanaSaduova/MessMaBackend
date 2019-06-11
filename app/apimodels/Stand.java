5b3c363f4dcbb203e6a9b3dbe86a7b5d2be5b933
Besart Laci <b.laci@mymovie.guru>
2019-06-11 12:34:47 +0200

    fix: String to Int for Stand fk_id_grid_point

--- app/apimodels/Stand.java
+++ app/apimodels/Stand.java
@@ -43,7 +43,7 @@ public class Stand   extends Model {
 
 
   @Column(name="fk_id_grid_point")
-  private String fk_id_grid_point;
+  private int fk_id_grid_point;
 
   @JsonProperty("GridPoint")
   private GridPoint gridPoint = null;
@@ -58,13 +58,13 @@ public class Stand   extends Model {
     List<Stand> standList = Stand.find.all();
 
     for (Stand stand: standList) {
-      stand.gridPoint = GridPoint.getByID(stand.fk_id_grid_point);
+      stand.gridPoint = GridPoint.getByID(Integer.toString(stand.fk_id_grid_point));
     }
 
     return standList;
   }
 
-  public String getFk_id_grid_point(){
+  public int getFk_id_grid_point(){
     return this.fk_id_grid_point;
   }
 
@@ -150,8 +150,11 @@ public class Stand   extends Model {
     this.gridPoint = gridPoint;
   }
 
+  public void setFk_id_grid_point(int fk_id_grid_point) {
+    this.fk_id_grid_point = fk_id_grid_point;
+  }
 
-  public String getFkGridPointId(){return fk_id_grid_point;}
+  public int getFkGridPointId(){return fk_id_grid_point;}
 
   @Override
   public boolean equals(java.lang.Object o) {
